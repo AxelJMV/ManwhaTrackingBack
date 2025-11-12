5ff52b10306c163c6983db6d0bf8911e502d1889
sky <sky@skys-MacBook-Pro.local>
2025-11-11 21:37:35 -0600

    test: add base Spring Boot context test

---
+++ src/test/java/com/manwhas_api/manwhas/ManwhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manwhas_api.manwhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManwhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
